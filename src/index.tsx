6002ceb7939e8141f9765b8e35c162cbe03460f6
phev8 <phev8@users.noreply.github.com>
2021-10-21 09:55:48 +0200

    - add project dependencies - clean up some unused boilerplate code

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '@fontsource/open-sans';
+// import '@fontsource/open-sans/300.css'; // for light font
+import '@fontsource/open-sans/400-italic.css';
+import '@fontsource/open-sans/700.css';
+
+import 'bootstrap/dist/js/bootstrap.bundle';
+// import * as bootstrap from 'bootstrap';
+
+import { LoadingPlaceholder } from 'case-web-ui';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Suspense fallback={<LoadingPlaceholder
+      color="white"
+      minHeight="100vh"
+    />}>
+      <App />
+    </Suspense>
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
